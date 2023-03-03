37c3fec782c1ea20a6f9c010e3097285aedc7b76
lucas <lucas@lucas-Aspire-VX5-591G>
2023-03-02 22:37:45 -0300

    programa completo TI2

--- Soma/src/TI2/SomarDoisNumeros.java
+++ Soma/src/TI2/SomarDoisNumeros.java
@@ -1,8 +1,18 @@
 package TI2;
-
+import java.util.*;
 public class SomarDoisNumeros {
+	public static Scanner sc =  new Scanner (System.in);
 	public static void main (String args[])
 	{
-		System.out.println("Hello");
+		int num1, num2, soma;
+
+		System.out.println("Digite um numero");
+		num1=sc.nextInt();
+		System.out.println("Digite outro numero");
+		num2=sc.nextInt();
+		
+		soma = num1+num2;
+		
+		System.out.println("Soma: " + soma);
 	}
 }
